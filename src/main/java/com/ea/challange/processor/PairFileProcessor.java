191f99d6b0c65d61cec6e9c064ceb2c02c75e686
deyindra <Cpwr@1234>
2016-04-26 05:55:24 -0700

    Update class diagram

--- src/main/java/com/ea/challange/processor/PairFileProcessor.java
+++ src/main/java/com/ea/challange/processor/PairFileProcessor.java
@@ -53,52 +53,56 @@ public class PairFileProcessor extends AbstractProcessFile{
     protected synchronized void populate(String s) {
         if(s!=null && s.trim().length()!=0){
             //Make an entry of word list with empty LinkedList with RowColIndex
-            adjacentMap.put(s, new LinkedList<>());
-            //Split the word pair
-            String[] array = s.split(PropertyUtil.getInstance()
-                    .getStringPropertyValue(WORD_SPLITER_PROPERTY_KEY,
-                            DEFAULT_WORD_SEP));
-            // Assumption if the number of words is not equal to kAdjacentWords then do not process the line
-            if(array.length!=kAdjacentWords){
-                LOGGER.error(String.format("Line %s does not have %d adjacent words",s,kAdjacentWords));
-            }else{
-                //Take the 1st word from the word list
-                String firstWord = array[0];
-                Set<RowColIndex> firstRowColIndexSets = wordMap.get(firstWord);
-                //Basically first word is not found in the wordMap
-                if(firstRowColIndexSets==null || firstRowColIndexSets.isEmpty()){
-                    LOGGER.error(String.format("1st word %s is not found in the bigger file", firstWord));
-                }else{
-                    for(RowColIndex rowColIndex:firstRowColIndexSets){
-                        //else get row and col index of the 1st word
-                        final long rowIndex=rowColIndex.getRowIndex();
-                        final long colIndex = rowColIndex.getColIndex();
-                        int count=1;
-                        LinkedList<RowColIndex> secondLinkedList = new LinkedList<>();
-                        //Loop through all adjacent word and check if the adjacent word is present in the same order
-                        //or not
-                        for(;count<kAdjacentWords;count++){
-                            Set<RowColIndex> adjacentRowColIndex = wordMap.get(array[count]);
-                            RowColIndex newRowColIndex = new RowColIndex(rowIndex,colIndex+count);
-                            if(adjacentRowColIndex!=null && adjacentRowColIndex.contains(newRowColIndex)){
-                                secondLinkedList.add(newRowColIndex);
-                            }else{
-                                LOGGER.error(String.format("%s does not have adjacent row and col Index",array[count]));
-                                secondLinkedList.clear();
-                                break;
+            if(!adjacentMap.containsKey(s)) {
+                adjacentMap.put(s, new LinkedList<>());
+                //Split the word pair
+                String[] array = s.split(PropertyUtil.getInstance()
+                        .getStringPropertyValue(WORD_SPLITER_PROPERTY_KEY,
+                                DEFAULT_WORD_SEP));
+                // Assumption if the number of words is not equal to kAdjacentWords then do not process the line
+                if (array.length != kAdjacentWords) {
+                    LOGGER.error(String.format("Line %s does not have %d adjacent words", s, kAdjacentWords));
+                } else {
+                    //Take the 1st word from the word list
+                    String firstWord = array[0];
+                    Set<RowColIndex> firstRowColIndexSets = wordMap.get(firstWord);
+                    //Basically first word is not found in the wordMap
+                    if (firstRowColIndexSets == null || firstRowColIndexSets.isEmpty()) {
+                        LOGGER.error(String.format("1st word %s is not found in the bigger file", firstWord));
+                    } else {
+                        for (RowColIndex rowColIndex : firstRowColIndexSets) {
+                            //else get row and col index of the 1st word
+                            final long rowIndex = rowColIndex.getRowIndex();
+                            final long colIndex = rowColIndex.getColIndex();
+                            int count = 1;
+                            LinkedList<RowColIndex> secondLinkedList = new LinkedList<>();
+                            //Loop through all adjacent word and check if the adjacent word is present in the same order
+                            //or not
+                            for (; count < kAdjacentWords; count++) {
+                                Set<RowColIndex> adjacentRowColIndex = wordMap.get(array[count]);
+                                RowColIndex newRowColIndex = new RowColIndex(rowIndex, colIndex + count);
+                                if (adjacentRowColIndex != null && adjacentRowColIndex.contains(newRowColIndex)) {
+                                    secondLinkedList.add(newRowColIndex);
+                                } else {
+                                    LOGGER.error(String.format("%s does not have adjacent row and col Index", array[count]));
+                                    secondLinkedList.clear();
+                                    break;
+                                }
                             }
-                        }
-                        //if the 2nd linkedList is not empty
-                        if(!secondLinkedList.isEmpty()){
-                            //add the 1st row col Index
-                            secondLinkedList.addFirst(rowColIndex);
-                            LinkedList<LinkedList<RowColIndex>> linkedLists = adjacentMap.get(s);
-                            LinkedList<RowColIndex> newLinkedList = new LinkedList<>();
-                            newLinkedList.addAll(secondLinkedList);
-                            linkedLists.add(newLinkedList);
+                            //if the 2nd linkedList is not empty
+                            if (!secondLinkedList.isEmpty()) {
+                                //add the 1st row col Index
+                                secondLinkedList.addFirst(rowColIndex);
+                                LinkedList<LinkedList<RowColIndex>> linkedLists = adjacentMap.get(s);
+                                LinkedList<RowColIndex> newLinkedList = new LinkedList<>();
+                                newLinkedList.addAll(secondLinkedList);
+                                linkedLists.add(newLinkedList);
+                            }
                         }
                     }
                 }
+            }else{
+                LOGGER.warn(String.format("%s is duplicate line... hence ignored processing", s));
             }
         }else{
             LOGGER.warn("Empty line..... hence skipping");
