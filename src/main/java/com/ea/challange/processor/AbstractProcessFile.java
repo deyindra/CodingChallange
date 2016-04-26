13f01fc5bc4712dcef2665cccc45660b65c46976
deyindra <Cpwr@1234>
2016-04-26 06:31:44 -0700

    Update class diagram

--- src/main/java/com/ea/challange/processor/AbstractProcessFile.java
+++ src/main/java/com/ea/challange/processor/AbstractProcessFile.java
@@ -5,10 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
